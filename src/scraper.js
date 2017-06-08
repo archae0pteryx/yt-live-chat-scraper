3a8b5c7291368e45f1c999a5d895770f952982a2
archae0pteryx <jesus.christ@openmailbox.org>
2017-06-08 13:53:46 -0700

    writing to file

--- src/scraper.js
+++ src/scraper.js
@@ -1,4 +1,5 @@
 var colorizer = require('colorizer').create('Colorizer');
+var fs = require('fs')
 
 var casper = require("casper").create({
   viewportSize: {
@@ -7,7 +8,7 @@ var casper = require("casper").create({
   }
 });
 
-url = 'https://www.youtube.com/live_chat?v=7azsl4iguoU&is_popout=1'
+url = 'https://www.youtube.com/live_chat?v=qUaPaogT1Q8&is_popout=1'
 ua = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
 
 casper.start()
@@ -17,8 +18,8 @@ casper.thenOpen(url, function () {
   this.echo("Waiting 10s", 'INFO')
   this.wait(10000, function () {
     casper.capture('screen-capture.png')
-    this.echo('...waited')
-    this.echo('Captured Screen.')
+    this.echo('...waited', 'COMMENT')
+    this.echo('Captured Screen.', 'COMMENT')
   })
 })
 
@@ -32,6 +33,7 @@ var currentMessage = '';
       var nodes = document.querySelectorAll('yt-live-chat-text-message-renderer'),
           author = nodes[nodes.length - 1].querySelector('#author-name').textContent,
           message = nodes[nodes.length - 1].querySelector('#message').textContent;
+      // ytd-menu-navigation-item-renderer
       return {
         author: author,
         message: message
@@ -40,12 +42,15 @@ var currentMessage = '';
   });
 
   casper.then(function () {
+    this.echo('Running', 'INFO')
     if (post === null) {
       this.echo("Error with message", 'WARNING')
       return
     } else if (currentMessage !== post.message) {
       currentMessage = post.message;
-      this.echo(colorizer.colorize(post.author, 'WARNING') + ' ' + colorizer.colorize(post.message, 'COMMENT'))
+      this.echo(colorizer.colorize(post.author, 'WARNING') + ' | ' + colorizer.colorize(post.message, 'COMMENT'))
+      formattedMsg = (post.author + ' - ' + post.message + ',\n')
+      fs.write('log.txt', formattedMsg, 'a')
     } 
   });
 
